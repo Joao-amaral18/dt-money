6d1bdbfb55cb575c2bf7aa8d8072948f6dab3426
Joao-amaral18 <joaovitor.lima.amaral@gmail.com>
2022-06-07 14:19:07 -0300

    hook implementation

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionProvider } from "./hooks/useTransactions";
 
 import { GlobalStyles } from "./styles/global";
-
 
 
 export function App() {
@@ -23,11 +23,11 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionProvider>
       <Header onOpenNewTransactionModal={openNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={closeNewTransactionModal}/>
       <GlobalStyles />
-    </>
+    </TransactionProvider>
   );
 }
